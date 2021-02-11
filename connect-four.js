e944b1c87743d0ae3a4a7891a2fe9dcd6d8db0ae
miguel <miguelalvinflores@gmail.com>
2021-02-11 13:35:47 -0800

    Initialized; midde of connect four

---
+++ connect-four.js
@@ -0,0 +1,7 @@
+import { Game } from "./game.js";
+let game = undefined;
+window.addEventListener("DOMContentLoaded", () => {
+    if()
+    let newGameBtn = document.getElementById("new-game");
+    newGameBtn.disabled = false;
+})
