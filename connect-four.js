16f7e0e19643275d30335dac0298ea6a608ab5b1
Natalie Oldroyd <natoh18@gmail.com>
2021-02-11 15:03:02 -0700

    second commit

--- connect-four.js
+++ connect-four.js
@@ -1,7 +1,24 @@
 import { Game } from "./game.js";
 let game = undefined;
 window.addEventListener("DOMContentLoaded", () => {
-    if()
+    let name1 = document.querySelector('#player-1-name')
+    let name2= document.querySelector('#player-2-name')
     let newGameBtn = document.getElementById("new-game");
-    newGameBtn.disabled = false;
+
+
+    document.getElementById('form-holder').addEventListener('keyup', () => {
+        if(name1.value !== name2.value && (name1.value && name2.value)) {
+        newGameBtn.disabled = false;
+    } else {
+        newGameBtn.disabled=true
+    }
+    })
+
+    newGameBtn.addEventListener('click', () => {
+        game = new Game(name1.value, name2.value)
+        name1.value = ""
+        name2.value = ""
+
+    })
+
 })
